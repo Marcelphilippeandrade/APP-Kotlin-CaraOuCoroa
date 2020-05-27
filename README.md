f7b39676656eaf88f36be2f2cdba1cb383419503
Marcel Philippe Abreu Andrade <marcelpaa@gmail.com>
2020-05-27 20:04:03 -0300

    Comitando o projeto do app cara ou coroa.

---
+++ README.md
@@ -0,0 +1,12 @@
+# APP Android - Cara ou Coroa
+Este aplicativo é um jogo de cara ou coroa. Ele foi desenvolvido utilizando a linguagem Kotlin.
+
+# Objetivo:
+Construir um jogo simples de cara ou coroa.
+
+# Tecnologias utilizadas: 
+* Kotlin
+
+:grin: :lollipop: :computer:
+
+
---
+++ app/src/main/java/com/cursoandroidkotlin/marcelphilippe/caraoucoroa/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.cursoandroidkotlin.marcelphilippe.caraoucoroa
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        image_jogar.setOnClickListener {
+            val intent = Intent(applicationContext, ResultadoActivity::class.java)
+            val numero = Random().nextInt(2)
+            intent.putExtra("numero", numero)
+            startActivity(intent)
+        }
+    }
+}
